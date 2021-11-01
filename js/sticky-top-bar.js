7fc654b572de0ec8882f003224bc4c335c73a2c0
John Turesson <john@hejare.se>
2021-11-01 17:48:48 +0100

    Header scroll fix + Accessible header + Responsive layout

--- js/sticky-top-bar.js
+++ js/sticky-top-bar.js
@@ -1,10 +1,20 @@
-window.addEventListener("scroll", function () {
-  var topBar = document.getElementById("top-bar");
-  topBar.classList.toggle("sticky", window.scrollY > 25)
-  // Hide away the top bar after some scrolling and shows it again when scroll up starts.
-  if (window.scrollY > 800) {
-    document.getElementById("top-bar").style.top = "-21vh";
-  } else {
-    document.getElementById("top-bar").style.top = "0";
-  }
-})
+(function () {
+  var prevScroll;
+
+  window.addEventListener("scroll", function () {
+    if (typeof prevScroll === 'undefined') {
+      prevScroll = window.scrollY;
+    }
+    
+    var topBar = document.getElementById("top-bar");
+    topBar.classList.toggle("sticky", window.scrollY > 25)
+    
+    // Hide away the top bar after some scrolling and shows it again when scroll up starts.
+    if (window.scrollY < 800 || prevScroll >= window.scrollY) {
+      document.getElementById("top-bar").style.top = "0";
+    } else {
+      document.getElementById("top-bar").style.top = "-21vh";
+    }
+    prevScroll = window.scrollY;
+  })
+})();
